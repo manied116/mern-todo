bad3bb524bcedb88ab5f29eca5b20f139c51c2d2
manied116 <36584580+manied116@users.noreply.github.com>
2025-10-08 18:38:53 +0000

    learn node js

---
+++ data.js
@@ -0,0 +1,5 @@
+names = ['a','b','c','d']
+marks = [22,23,24,25]
+
+console.log("test run")
+module.exports = {names,marks}
---
+++ module.js
@@ -0,0 +1,9 @@
+const data = require('./data')
+//or
+const {names,marks} = require('./data')
+const os = require('os')
+console.log(data.names)
+// console.log(data.marks)
+console.log(marks)
+
+console.log(os.homedir())
